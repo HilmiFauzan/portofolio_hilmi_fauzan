77474d3c4b9e1b2d7564f3f3f628839237bd9eb1
Hilmi Fauzan <hilmifauzan170@gmail.com>
2022-06-20 17:38:55 +0700

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0d9488',
+        secondary: '#64748b',
+        dark: '#115e59',
+      },
+      screens:{
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
